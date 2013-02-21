d4cd48c8cbca402e60a16f3d20cbe0528f7e4a9a
cmacfarl <cmacfarl@cmacfarl-VirtualBox.(none)>
2013-02-21 15:13:11 -0800

    Lecture 4 assignments and programming skeletons.

---
+++ lecture04/print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/*
+ * Implement the entire function print()
+ */
+
+int main()
+{
+	int arr[] = {4, 7, 2, 3, 5};
+
+	print(arr, 5);
+}
---
+++ lecture04/sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int sum(/* Replace this comment with two parameters */)
+{
+	/*
+     * Write the function body
+	 */
+}
+
+
+int main()
+{
+	int s;
+	int arr[] = /* Initialize your array with some values */
+
+	s = sum(arr, /* How many values did you initialize */);
+
+	std::cout << "Sum: " << s << "\n";
+}
